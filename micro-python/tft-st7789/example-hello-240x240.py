19cd7c2c2994a7f0ae0374c4636708ce33a358f4
Vitalii Panko <vitalii.panko@covantis.io>
2025-08-02 09:15:24 +0300

    add tft 1.3 inch 240x240

---
+++ micro-python/tft-st7789/example-hello-240x240.py
@@ -0,0 +1,42 @@
+from pyb import SPI, Pin
+
+import st7789py as st7789
+import vga2_16x32 as font
+import time
+
+# 1.3" TFT Display Module IPS LCD LED Screen 240X240 SPI Interface ST7789 Controller 3.3V
+
+width = 240
+height = 240
+
+# TFT       Nicla Vision
+# ----------------------
+# LED/BLK   VDDIO_EXT
+# SCK       SCLK
+# SDA       COPI
+# A0/DC     D1
+# RESET     D0
+# СS        CS
+# GND       GND
+# VCC       VDDIO_EXT
+
+cs = Pin("CS", Pin.OUT_OD)
+reset = Pin("D0", Pin.OUT_PP)
+dc = Pin("D1", Pin.OUT_PP)
+
+spi = SPI(4, SPI.MASTER, baudrate=int(1000000000 / 66), polarity=1, phase=1)
+
+
+# Initialize display
+display = st7789.ST7789(spi, width, height, reset=reset, dc=dc, rotation=0)
+display.sleep_mode(False)
+
+display.fill(st7789.BLACK)
+display.text(font, "SPI ST7789", 10, 10, color=0xF800)
+display.text(font, "Display", 10, 50, color=0x07E0)
+display.text(font, "Example", 10, 80, color=0x001F)
+display.text(font, "Say hello!", 10, 170)
+time.sleep(1)
+display.text(font, "Say ...   ", 10, 170)
+time.sleep(1)
+display.text(font, "Say hello!", 10, 170)
